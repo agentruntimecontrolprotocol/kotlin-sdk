a66d3c2134e6ba82fe22a48dab0f53fa84b9d847
Nick Ficano <nficano@gmail.com>
2026-05-22 12:01:38 -0400

    remove unused ResultSet import (no-unused-imports ktlint violation)

--- lib/src/main/kotlin/dev/arcp/store/EventLog.kt
+++ lib/src/main/kotlin/dev/arcp/store/EventLog.kt
@@ -14,7 +14,6 @@ import kotlinx.serialization.json.JsonElement
 import java.nio.file.Path
 import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.ResultSet
 import java.sql.SQLException
 import java.util.concurrent.atomic.AtomicBoolean
 
